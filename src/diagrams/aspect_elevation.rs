ea6b238d80412673ce8f374bd9187b5c6548e597
Luke Frisken <l.frisken@gmail.com>
2023-01-22 08:11:06 +1100

    Aspect elevation chart svg

--- src/diagrams/aspect_elevation.rs
+++ src/diagrams/aspect_elevation.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashSet, str::FromStr};
+use std::{
+    borrow::Cow,
+    collections::{HashMap, HashSet},
+    str::FromStr,
+};
 
 use axum::{
     extract::Query,
@@ -6,9 +10,11 @@ use axum::{
     response::IntoResponse,
 };
 use eyre::Context;
+use once_cell::sync::Lazy;
+use regex::{Captures, Regex};
 use serde::Deserialize;
 
-#[derive(Debug, PartialEq, Eq, Hash)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
 pub enum Aspect {
     N,
     NE,
@@ -18,6 +24,35 @@ pub enum Aspect {
     SW,
     W,
     NW,
+}
+
+impl Aspect {
+    fn id(&self) -> &'static str {
+        match self {
+            Aspect::N => "n",
+            Aspect::NE => "ne",
+            Aspect::E => "e",
+            Aspect::SE => "se",
+            Aspect::S => "s",
+            Aspect::SW => "sw",
+            Aspect::W => "w",
+            Aspect::NW => "nw",
+        }
+    }
+
+    #[cfg(test)]
+    fn enumerate() -> &'static [Self] {
+        &[
+            Aspect::N,
+            Aspect::NE,
+            Aspect::E,
+            Aspect::SE,
+            Aspect::S,
+            Aspect::SW,
+            Aspect::W,
+            Aspect::NW,
+        ]
+    }
 }
 
 impl FromStr for Aspect {
@@ -49,7 +84,14 @@ fn comma_separated_to_vec(comma_separated: String) -> eyre::Result<HashSet<Aspec
     comma_separated
         .split(',')
         .into_iter()
-        .map(|value| value.trim().parse::<Aspect>())
+        .filter_map(|aspect_str| {
+            let aspect_str = aspect_str.trim();
+            if aspect_str.is_empty() {
+                None
+            } else {
+                Some(aspect_str.parse::<Aspect>())
+            }
+        })
         .collect()
 }
 
@@ -87,15 +129,94 @@ pub struct AspectElevationQuery {
     sub_alpine: Option<String>,
 }
 
+const SVG_TEMPLATE: &str = include_str!("./aspect_elevation.svg");
+const FILLED_COLOUR: &str = "#276fdcff";
+static PATH_ID_RE: Lazy<Regex> = Lazy::new(|| {
+    Regex::new(
+        r#"<path\s*style="(?P<fill>fill:(?P<colour>#ffffff);)([^/])*id="(?P<id>[a-z\-]+)"\s*[/]>"#,
+    )
+    .expect("Unable to compile svg path id regex")
+});
+
+fn generate_svg(aspect_elevation: AspectElevation) -> Cow<'static, str> {
+    let high_alpine_ids = aspect_elevation.high_alpine.into_iter().map(|aspect| {
+        let id = aspect.id();
+        format!("high-alpine-{id}")
+    });
+    let alpine_ids = aspect_elevation.alpine.into_iter().map(|aspect| {
+        let id = aspect.id();
+        format!("alpine-{id}")
+    });
+    let sub_alpine_ids = aspect_elevation.sub_alpine.into_iter().map(|aspect| {
+        let id = aspect.id();
+        format!("sub-alpine-{id}")
+    });
+
+    let colour_map: HashMap<String, &str> = high_alpine_ids
+        .chain(alpine_ids)
+        .chain(sub_alpine_ids)
+        .map(|id| (id, FILLED_COLOUR))
+        .collect();
+
+    PATH_ID_RE.replace_all(SVG_TEMPLATE, |captures: &Captures| {
+        let id = captures.name("id").unwrap().as_str();
+        let captured_string = captures.get(0).unwrap().as_str();
+        if let Some(colour) = colour_map.get(id) {
+            let fill_group_string = captures.name("fill").unwrap().as_str();
+            captured_string.replace(fill_group_string, &format!("fill:{colour};"))
+        } else {
+            captured_string.to_string()
+        }
+    })
+}
+
 pub async fn svg_handler(
     Query(aspect_elevation_query): Query<AspectElevationQuery>,
 ) -> axum::response::Result<impl IntoResponse> {
     let mut headers = HeaderMap::new();
     // headers.insert(header::CONTENT_TYPE, "image/svg+xml".parse().unwrap());
-    headers.insert(header::CONTENT_TYPE, "text/plain".parse().unwrap());
+    headers.insert(header::CONTENT_TYPE, "image/svg+xml".parse().unwrap());
     let aspect_elevation = AspectElevation::try_from(aspect_elevation_query).map_err(|error| {
         tracing::error!("{:?}", error);
         StatusCode::INTERNAL_SERVER_ERROR
     })?;
-    Ok((headers, format!("{aspect_elevation:?}")))
+    Ok((headers, generate_svg(aspect_elevation)))
+}
+
+#[cfg(test)]
+mod test {
+    use std::collections::HashSet;
+
+    use super::{generate_svg, Aspect, AspectElevation};
+
+    #[test]
+    fn test_generate_svg_empty() {
+        let svg = generate_svg(AspectElevation {
+            high_alpine: vec![].into_iter().collect(),
+            alpine: vec![].into_iter().collect(),
+            sub_alpine: vec![].into_iter().collect(),
+        });
+        insta::assert_snapshot!(svg);
+    }
+
+    #[test]
+    fn test_generate_svg_all_aspects() {
+        let all_aspects: HashSet<Aspect> = Aspect::enumerate().into_iter().cloned().collect();
+        let svg = generate_svg(AspectElevation {
+            high_alpine: all_aspects.clone(),
+            alpine: all_aspects.clone(),
+            sub_alpine: all_aspects.clone(),
+        });
+        insta::assert_snapshot!(svg);
+    }
+
+    #[test]
+    fn test_generate_svg_alpine_n_w() {
+        let svg = generate_svg(AspectElevation {
+            high_alpine: HashSet::default(),
+            alpine: vec![Aspect::N, Aspect::NW].into_iter().collect(),
+            sub_alpine: HashSet::default(),
+        });
+        insta::assert_snapshot!(svg);
+    }
 }
--- src/diagrams/mod.rs
+++ src/diagrams/mod.rs
@@ -7,5 +7,5 @@ pub fn router() -> Router {
     Router::new()
         .route("/elevation_hazard.svg", get(elevation_hazard::svg_handler))
         .route("/elevation_hazard.png", get(elevation_hazard::png_handler))
-        .route("/aspect_elevation", get(aspect_elevation::svg_handler))
+        .route("/aspect_elevation.svg", get(aspect_elevation::svg_handler))
 }
