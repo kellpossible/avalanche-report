a1cc3302ce0bbbca957adea92577faf00bac8341
Luke Frisken <l.frisken@gmail.com>
2023-02-26 11:35:55 +1100

    Work in progress parsing forecast spreadsheet

---
+++ forecast-spreadsheet/src/options.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+
+use serde::Deserialize;
+
+use crate::{serde::string, SheetCellPosition};
+
+#[derive(Deserialize)]
+pub struct Options {
+    /// What version of the spreadsheet these options are for.
+    #[serde(with = "string")]
+    pub schema_version: crate::Version,
+    pub template_version: Version,
+    pub language: Language,
+    pub area: Area,
+}
+
+#[derive(Deserialize)]
+pub struct Version {
+    pub position: SheetCellPosition,
+}
+
+#[derive(Deserialize)]
+pub struct Area {
+    pub position: SheetCellPosition,
+    /// A map from area name to area identifier.
+    pub map: HashMap<String, String>
+}
+
+#[derive(Deserialize)]
+pub struct Language {
+    pub position: SheetCellPosition,
+    /// A map from language name (in the spreadsheet) to language identifier.
+    pub map: HashMap<String, unic_langid::LanguageIdentifier>
+}
