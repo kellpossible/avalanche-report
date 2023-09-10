7f48a659ae3bf6b60acf7d058271cc578faff7ce
Luke Frisken <l.frisken@gmail.com>
2023-09-11 08:22:09 +1200

    Work in progress elevation band calculations from DEM

---
+++ geo/debug/src/main.rs
@@ -0,0 +1,26 @@
+use plotly::{Image, Configuration};
+use show_image::{create_window, ImageView, ImageInfo, WindowOptions};
+
+
+#[show_image::main]
+pub fn main() -> eyre::Result<()> {
+    let pixels = geo::render_elevations("../fixtures/ASTGTMV003_N42E044_dem.tif").unwrap();
+    // let pixels_rgb = pixels.mapv(|value| (value, value, value));
+    // let mut plot = plotly::Plot::new();
+    // let image = Image::new(pixels_rgb).color_model(plotly::image::ColorModel::RGB);
+    // plot.add_trace(image);
+    // plot.set_configuration(Configuration::default().fill_frame(true));
+    // plot.show();
+
+
+    let shape = pixels.shape();
+    let view = ImageView::new(
+        ImageInfo::mono8(shape[0] as u32, shape[1] as u32),
+        pixels.as_slice_memory_order().unwrap(),
+    );
+    let window = create_window("Mountains", WindowOptions::default()).unwrap();
+    window.set_image("mountains", view).unwrap();
+    window.wait_until_destroyed()?;
+    Ok(())
+}
+
---
+++ geo/src/lib.rs
@@ -0,0 +1,49 @@
+use std::path::Path;
+
+use eyre::{Context, ContextCompat};
+use gdal::raster::{ResampleAlg, StatisticsAll};
+use ndarray::Array2;
+
+pub fn render_elevations<P: AsRef<Path>>(path: P) -> eyre::Result<Array2<u8>> {
+    let path = path.as_ref();
+    let dataset = gdal::Dataset::open(
+        path.as_os_str()
+            .to_str()
+            .wrap_err("Unable to convert path to str")?,
+    )
+    .wrap_err("Error opening")?;
+    dbg!(dataset.raster_count());
+    dbg!(dataset.raster_size());
+
+    let scale = 1.0;
+
+    let raster = dataset.rasterband(1).wrap_err("Error getting raster")?;
+    let array_size = (
+        (raster.size().0 as f64 * scale) as usize,
+        (raster.size().1 as f64 * scale) as usize,
+    );
+    dbg!(array_size);
+    let array = raster
+        .read_as_array::<i32>((0, 0), raster.size(), array_size, Some(ResampleAlg::Cubic))
+        .wrap_err("Error readig raster into array")?;
+    dbg!(array.shape());
+    let stats: StatisticsAll = raster
+        .get_statistics(true, true)?
+        .wrap_err("No statistics")?;
+
+    let proj = dataset.projection();
+    let xform = dataset.geo_transform()?;
+    let spatial_ref = dataset.spatial_ref()?;
+    dbg!(&spatial_ref);
+    dbg!(&xform);
+    dbg!(&proj);
+    dbg!(&stats);
+
+    // // Map to 256 range
+    // let image =
+    //     array.mapv(|x| (((x as f64) - stats.min) * (256.0 / (stats.max - stats.min))) as u8);
+
+    let image = array.mapv(|h| if h > 1000 && h < 2000 { 255u8 } else { 0u8 });
+
+    Ok(image)
+}
